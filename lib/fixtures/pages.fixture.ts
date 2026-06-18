e105d1d0078aece5e0070d259f1efd600b70ede2
pendragon888 <pendragon888@hotmail.com>
2026-06-18 12:07:35 +0100

    Add test & fixtures and for About page

--- lib/fixtures/pages.fixture.ts
+++ lib/fixtures/pages.fixture.ts
@@ -1,10 +1,12 @@
 import { NavigationPage } from "@pages/navigation/navigation.page";
 import { LandingPage } from "@pages/landing/landing.page";
+import { AboutPage } from "@pages/about/about.page";
 import { test as baseTest } from "@playwright/test";
 
 type MyPages = {
   navigationPage: NavigationPage;
   landingPage: LandingPage;
+  aboutPage: AboutPage;
 };
 
 export const test = baseTest.extend<MyPages>({
@@ -13,6 +15,9 @@ export const test = baseTest.extend<MyPages>({
   },
     landingPage: async ({ page }, use) => {
     await use(new LandingPage(page));
+  },
+    aboutPage: async ({ page }, use) => {
+    await use(new AboutPage(page));
   }
 });
 
---
+++ lib/pages/about/about.page.ts
@@ -0,0 +1,17 @@
+import { type Locator, type Page} from "playwright";
+
+export class AboutPage {
+    readonly page: Page
+    readonly aboutSection: Locator
+    readonly ourStory: Locator
+
+    constructor(page:Page) {
+        this.page = page
+        this.aboutSection = page.locator('body')
+        this.ourStory = page.getByRole('paragraph')
+    }
+
+    async goto(){
+        await this.page.goto(process.env.BASE_URL+'about')
+    }
+}
