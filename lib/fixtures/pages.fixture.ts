f24ffc8d96affd9792e140a699efb0d39563b07c
pendragon888 <pendragon888@hotmail.com>
2026-06-24 13:31:41 +0100

    Add fixture for remaining pages

--- lib/fixtures/pages.fixture.ts
+++ lib/fixtures/pages.fixture.ts
@@ -1,12 +1,22 @@
 import { NavigationPage } from "@pages/navigation/navigation.page";
 import { LandingPage } from "@pages/landing/landing.page";
+import { ForPatientsPage } from "@pages/forPatients/forPatients.page";
+import { ForOrganisationsPage } from "@pages/forOrganisations/forOrganisations.page";
+import { NewsPage} from '@pages/news/news.page'
 import { AboutPage } from "@pages/about/about.page";
+import { JoinTheTeamPage } from "@pages/joinTheTeam/joinTheTeam.page";
+import { ContactUsPage } from "@pages/contactUs/contactUs.page";
 import { test as baseTest } from "@playwright/test";
 
 type MyPages = {
   navigationPage: NavigationPage;
   landingPage: LandingPage;
+  forPatientsPage: ForPatientsPage;
+  forOrganisationsPage: ForOrganisationsPage;
+  newsPage: NewsPage;
   aboutPage: AboutPage;
+  joinTheTeamPage: JoinTheTeamPage;
+  contactUsPage: ContactUsPage;
 };
 
 export const test = baseTest.extend<MyPages>({
@@ -16,8 +26,23 @@ export const test = baseTest.extend<MyPages>({
     landingPage: async ({ page }, use) => {
     await use(new LandingPage(page));
   },
+    forPatientsPage: async ({ page }, use) => {
+    await use(new ForPatientsPage(page));
+  },
+    forOrganisationsPage: async ({ page }, use) => {
+    await use(new ForOrganisationsPage(page));
+  },
+    newsPage: async ({ page }, use) => {
+    await use(new NewsPage(page));
+  },
     aboutPage: async ({ page }, use) => {
     await use(new AboutPage(page));
+  },
+    joinTheTeamPage: async ({ page }, use) => {
+    await use(new JoinTheTeamPage(page));
+  },
+    contactUsPage: async ({ page }, use) => {
+    await use(new ContactUsPage(page));
   }
 });
 
