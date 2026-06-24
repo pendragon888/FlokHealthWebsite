f2bc8c4e8a56b5aac3d8056c3bcdb871892ae6fc
pendragon888 <pendragon888@hotmail.com>
2026-06-24 13:34:07 +0100

    Adding page elements and test to 'For Patients'

---
+++ lib/pages/forPatients/forPatients.page.ts
@@ -0,0 +1,23 @@
+import { type Locator, type Page} from "playwright";
+
+export class ForPatientsPage {
+    readonly page: Page
+    readonly checkAvailabilityLink: Locator
+    readonly personalCare: Locator
+    readonly videoPlayer: Locator
+    readonly videoPlayerPlay : Locator
+    readonly videoPlayerPause : Locator
+
+    constructor(page:Page) {
+        this.page = page
+        this.checkAvailabilityLink = page.getByRole('link', { name: 'Check availability' })
+        this.personalCare = page.locator('#personal-care')
+        this.videoPlayer = page.locator('iframe[title="Flok Health Hero Video"]')
+        this.videoPlayerPlay = page.locator('iframe[title="Flok Health Hero Video"]').contentFrame().locator('#player').contentFrame().getByRole('button', { name: 'Play' })
+        this.videoPlayerPause = this.videoPlayer.contentFrame().locator('#player').contentFrame().getByRole('button', { name: 'Pause' })
+    }
+
+    async goto(){
+        await this.page.goto(process.env.BASE_URL+'forpatients')
+    }
+}
---
+++ tests/forPatients/forPatients.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@fixtures/pages.fixture'
+
+test.describe("Visit For Patients page for Flok Health", ()=>{
+    test.beforeEach(async ({ forPatientsPage }) => {
+        await forPatientsPage.goto()
+    })
+
+    test('Validate For Patients page present, ', async ({ forPatientsPage }) => {
+
+        await expect(forPatientsPage.checkAvailabilityLink.first()).toBeVisible()
+        await expect(forPatientsPage.personalCare).toContainText('The help you need, when you need itWe\'re with you every step of the way, so you can get back to what matters most.')
+    })
+
+    test('Validate video player, ', async ({ forPatientsPage }) => {
+        await expect(forPatientsPage.videoPlayer).toBeVisible()
+        await forPatientsPage.videoPlayerPlay.click()
+    })
+})
