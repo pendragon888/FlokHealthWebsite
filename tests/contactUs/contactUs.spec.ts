e2ff06381d594a0f612017b2289be77a45176df2
pendragon888 <pendragon888@hotmail.com>
2026-07-06 18:38:10 +0100

    Job application form has been replaced with an email link

--- tests/contactUs/contactUs.spec.ts
+++ tests/contactUs/contactUs.spec.ts
@@ -1,25 +1,27 @@
 import { test, expect } from '@fixtures/pages.fixture'
+import { formData } from '@datafactory/jobApplicationData.json'
 
 test.describe("Visit Contact Us page for Flok Health", ()=>{
     test.beforeEach(async ({ contactUsPage }) => {
         await contactUsPage.goto()
     })
 
-    test('Validate Contact Us page present, ', async ({ contactUsPage }) => {
+    test('Validate Contact Us page present', async ({ contactUsPage }) => {
         await expect(contactUsPage.generalEnquiryButton).toBeVisible();
         await expect(contactUsPage.haveAQuestionText).toBeVisible()
         await expect(contactUsPage.haveAQuestionText).toContainText("Have a question or need support? Start by choosing what you’d like to get in touch about, send us a message, and we’ll get back to you as soon as we can.")
     })
 
-    test('Fill out Job Application Form, ', async ({ contactUsPage }) => {
+    // Job application form has been removed
+    test.skip('Fill out Job Application Form using test data', async ({ contactUsPage }) => {
         await expect(contactUsPage.jobApplicationButton).toBeVisible();
         await expect(contactUsPage.jobApplicationButton).toContainText('Job application');
         await contactUsPage.jobApplicationButton.click()
-        await (contactUsPage.roleSelector).selectOption('Senior/Staff System Test Engineer')
-        await contactUsPage.firstNameField.fill('Kevin');
-        await contactUsPage.surnameField.fill('Dang');
-        await contactUsPage.emailField.fill('pendragon888@hotmail.com');
-        await contactUsPage.messageField.fill('Application for Senior/Staff System Test Engineer');
+        await (contactUsPage.roleSelector).selectOption(formData.role)
+        await contactUsPage.firstNameField.fill(formData.firstName);
+        await contactUsPage.surnameField.fill(formData.surname);
+        await contactUsPage.emailField.fill(formData.email);
+        await contactUsPage.messageField.fill(formData.message);
         await contactUsPage.confirmationCheckbox.check()
     })
 })
