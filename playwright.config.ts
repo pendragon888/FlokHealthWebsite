bbd4690908f4977bd163d1b2c7c5f2182398343a
pendragon888 <pendragon888@hotmail.com>
2026-07-13 17:04:55 +0100

    Add screenshot to failed test

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    screenshot: 'only-on-failure' // Automatically capture screenshot when a test fails
   },
 
   /* Configure projects for major browsers */
