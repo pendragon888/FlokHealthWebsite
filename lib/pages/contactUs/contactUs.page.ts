a2f8c5a8f17825be33320b70843dd9a2fa8a0aca
pendragon888 <pendragon888@hotmail.com>
2026-07-13 22:07:03 +0100

    Add page locators for general and patient enquiry forms, add tests to enter data into forms n.b. Playwright will not be able to bypass reCaptcha

--- lib/pages/contactUs/contactUs.page.ts
+++ lib/pages/contactUs/contactUs.page.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page} from "playwright";
+import { type Locator, type Page} from "playwright"
 
 export class ContactUsPage {
     readonly page: Page
@@ -6,13 +6,31 @@ export class ContactUsPage {
     readonly generalEnquiryButton: Locator
     readonly patientEnquiryButton: Locator
     readonly jobApplicationButton: Locator
-    readonly roleSelector: Locator
-    readonly firstNameField: Locator
-    readonly surnameField: Locator
-    readonly emailField: Locator
-    readonly messageField: Locator
-    readonly confirmationCheckbox: Locator
+    readonly recaptchaCheckbox: Locator
     readonly submitButton: Locator
+
+    // General enquiry
+    readonly generalEnquiryForm: Locator
+    readonly GEFirstNameField: Locator
+    readonly GESurnameField: Locator
+    readonly GEEmailField: Locator
+    readonly GEMessageField: Locator
+
+    // Patient enquiry
+    readonly patientEnquiryForm: Locator
+    readonly PEFirstNameField: Locator
+    readonly PESurnameField: Locator
+    readonly PEEmailField: Locator
+    readonly PEMessageField: Locator
+
+    // Job Application
+    readonly JARoleSelector: Locator
+    readonly JAFirstNameField: Locator
+    readonly JASurnameField: Locator
+    readonly JAEmailField: Locator
+    readonly JAMessageField: Locator
+    readonly JAConfirmationCheckbox: Locator
+
 
     constructor(page:Page) {
         this.page = page
@@ -20,13 +38,30 @@ export class ContactUsPage {
         this.generalEnquiryButton = page.getByRole('tab', { name: 'General enquiry' })
         this.patientEnquiryButton = page.getByRole('tab', { name: 'Patient enquiry' })
         this.jobApplicationButton = page.getByRole('tab', { name: 'Job application' })
-        this.roleSelector = page.getByRole('combobox')
-        this.firstNameField = page.locator('#name-career')
-        this.surnameField = page.locator('#Surname-career')
-        this.emailField = page.locator('#Email-career')
-        this.messageField = page.locator('#Message-career')
-        this.confirmationCheckbox = page.locator('.w-checkbox-input')
+        this.recaptchaCheckbox = page.locator('#recaptcha-anchor')
         this.submitButton = page.getByRole('button', { name: 'Submit' })
+
+        // General enquiry
+        this.generalEnquiryForm = page.locator('[aria-label="General enquiry form"]')
+        this.GEFirstNameField = page.locator('#name')
+        this.GESurnameField = page.locator('#Surname')
+        this.GEEmailField = page.locator('#Email')
+        this.GEMessageField = page.locator('#Message')
+
+        // Patient enquiry
+        this.patientEnquiryForm = page.locator('[aria-label="Patient support form"]')
+        this.PEFirstNameField = page.locator('#name-support')
+        this.PESurnameField = page.locator('#Surname-support')
+        this.PEEmailField = page.locator('#Email-support')
+        this.PEMessageField = page.locator('#Message-support')
+
+        // Job application (this could be removed, since the section no longer exists)
+        this.JARoleSelector = page.getByRole('combobox')
+        this.JAFirstNameField = page.locator('#name-career')
+        this.JASurnameField = page.locator('#Surname-career')
+        this.JAEmailField = page.locator('#Email-career')
+        this.JAMessageField = page.locator('#Message-career')
+        this.JAConfirmationCheckbox = page.locator('.w-checkbox-input')
        }
 
     async goto(){
--- tests/contactUs/contactUs.spec.ts
+++ tests/contactUs/contactUs.spec.ts
@@ -7,21 +7,42 @@ test.describe("Visit Contact Us page for Flok Health", ()=>{
     })
 
     test('Validate Contact Us page present', async ({ contactUsPage }) => {
-        await expect(contactUsPage.generalEnquiryButton).toBeVisible();
+        await expect(contactUsPage.generalEnquiryButton).toBeVisible()
         await expect(contactUsPage.haveAQuestionText).toBeVisible()
         await expect(contactUsPage.haveAQuestionText).toContainText("Have a question or need support? Start by choosing what you’d like to get in touch about, send us a message, and we’ll get back to you as soon as we can.")
     })
 
+    test('Fill out General Enquiry form', async ({ contactUsPage}) => {
+        await expect(contactUsPage.generalEnquiryButton).toBeVisible()
+        await expect(contactUsPage.generalEnquiryButton).toContainText('General enquiry')
+        await expect(contactUsPage.generalEnquiryForm).toBeVisible()
+        await contactUsPage.GEFirstNameField.fill(formData.firstName)
+        await contactUsPage.GESurnameField.fill(formData.surname)
+        await contactUsPage.GEEmailField.fill(formData.email)
+        await contactUsPage.GEMessageField.fill(formData.message)
+    })
+
+    test('Fill out Patient Enquiry form', async ({ contactUsPage}) => {
+        await expect(contactUsPage.patientEnquiryButton).toBeVisible()
+        await expect(contactUsPage.patientEnquiryButton).toContainText('Patient enquiry')
+        await (contactUsPage.patientEnquiryButton).click()
+        await expect(contactUsPage.patientEnquiryForm).toBeVisible()
+        await contactUsPage.PEFirstNameField.fill(formData.firstName)
+        await contactUsPage.PESurnameField.fill(formData.surname)
+        await contactUsPage.PEEmailField.fill(formData.email)
+        await contactUsPage.PEMessageField.fill(formData.message)
+    })
+
     // Job application form has been removed
     test.skip('Fill out Job Application Form using test data', async ({ contactUsPage }) => {
-        await expect(contactUsPage.jobApplicationButton).toBeVisible();
-        await expect(contactUsPage.jobApplicationButton).toContainText('Job application');
+        await expect(contactUsPage.jobApplicationButton).toBeVisible()
+        await expect(contactUsPage.jobApplicationButton).toContainText('Job application')
         await contactUsPage.jobApplicationButton.click()
-        await (contactUsPage.roleSelector).selectOption(formData.role)
-        await contactUsPage.firstNameField.fill(formData.firstName);
-        await contactUsPage.surnameField.fill(formData.surname);
-        await contactUsPage.emailField.fill(formData.email);
-        await contactUsPage.messageField.fill(formData.message);
-        await contactUsPage.confirmationCheckbox.check()
+        await (contactUsPage.JARoleSelector).selectOption(formData.role)
+        await contactUsPage.JAFirstNameField.fill(formData.firstName)
+        await contactUsPage.JASurnameField.fill(formData.surname)
+        await contactUsPage.JAEmailField.fill(formData.email)
+        await contactUsPage.JAMessageField.fill(formData.message)
+        await contactUsPage.JAConfirmationCheckbox.check()
     })
 })
