1d1b4974b6aae6bc0ee079a33eb9c9d2d6df9da9
pendragon888 <pendragon888@hotmail.com>
2026-07-14 20:49:50 +0100

    Update test to confirm CheckAvailability navigates to the correct page

--- tests/forPatients/forPatients.spec.ts
+++ tests/forPatients/forPatients.spec.ts
@@ -9,10 +9,15 @@ test.describe("Visit For Patients page for Flok Health", ()=>{
         await expect(forPatientsPage.personalCare).toContainText('The help you need, when you need itWe\'re with you every step of the way, so you can get back to what matters most.')
     })
 
-    test('Validate Check availabiity link is present and directs to correct page', async ({ forPatientsPage, checkAvailabilityPage }) => {
+    test('Validate Check availabiity link is present and directs to correct page', async ({ forPatientsPage, page, context }) => {
         await expect(forPatientsPage.checkAvailabilityLink.first()).toBeVisible()
-        await forPatientsPage.checkAvailabilityLink.click()
-        await checkAvailabilityPage
+
+        context.waitForEvent('page')
+        const checkAvailabilityPromise = page.waitForEvent('popup');
+        await forPatientsPage.checkAvailabilityLink.first().click()
+        const newCheckAvailabilityPage = await checkAvailabilityPromise;
+        await newCheckAvailabilityPage.getByRole('textbox', { name: 'Postcode' }).fill('CB23 7EE');
+        await expect(newCheckAvailabilityPage).toHaveURL('https://clinic.flok.health/self-refer/coverage')
     })
 
     test('Validate Vimeo video player is present and plays', async ({ forPatientsPage }) => {
