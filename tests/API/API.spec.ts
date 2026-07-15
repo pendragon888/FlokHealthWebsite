1100c9f89255aca4392f5eb77656c21de6e3f9fe
pendragon888 <pendragon888@hotmail.com>
2026-07-15 17:45:46 +0100

    Save API data to file

--- tests/API/API.spec.ts
+++ tests/API/API.spec.ts
@@ -1,17 +1,29 @@
 import { test, expect } from '@fixtures/pages.fixture'
+import fs from 'fs'
+import path from 'path'
 
 test.describe('API testing', async () => {
     test.describe.configure({ retries:1 })
     test('GET GP Practices from API', async ({ request }) => {
-        const response = await request.get('https://clinic.flok.health/api/self-refer/pathway?postcode=CB23');
+        // Define a relative file path (relative to project root)
+        const relativeDir = path.join(__dirname, '../../.temp');
+        const fileName = 'gpPracticesFile.json';
+        const gpPracticesFile = path.join(relativeDir, fileName);
+        const response = await request.get('https://clinic.flok.health/api/self-refer/pathway?postcode=CB23')
+        if (!response.ok()) {
+            throw new Error(`Request failed with status ${response.status()}`)
+        }
         expect(response.status()).toBe(200)
         const body = await response.json()
         expect(body).toHaveProperty('gp_list')
         expect(body).toBeTruthy()
+
+        // Save response to JSON file
+        await fs.writeFileSync(gpPracticesFile, JSON.stringify({ body }, null, 2));
     })
 
     test('GET GP Practices from API without postcode throws 400 status code error', async ({ request }) => {
-        const response = await request.get('https://clinic.flok.health/api/self-refer/pathway?postcode=');
+        const response = await request.get('https://clinic.flok.health/api/self-refer/pathway?postcode=')
         expect(response.status()).toBe(400)
     })
 })
